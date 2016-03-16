27cc10ea7279bed9c7d1cdb8d93425221504f60b
Julien Breux <julien.breux@gmail.com>
2016-03-16 11:39:25 +0100

    Add some docs

--- lib/speedflow/plugin/test.rb
+++ lib/speedflow/plugin/test.rb
@@ -1,10 +1,18 @@
 require 'speedflow/plugin/test/version'
 
+# Speedflow GEM, help you to boost your flow and keep your time
 module Speedflow
   module Plugin
     # Speedflow test plugin
     module Test
       class << self
+        # Public: Method missign
+        #
+        # Method used to catch others methods to test.
+        #
+        # *args - Some arguments :)
+        #
+        # Returns nothing.
         def method_missing(*args)
           action = args.first
           arguments = args.last
